f37b296684275b62b9ecc5a13a4f20e4cfb3f63f
Marchin <mmonterrosa94@gmail.com>
2018-08-21 17:35:25 -0300

    v0.1.1 Camera
    Add Camera scripts

---
+++ FPSVRails/Assets/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+	[SerializeField] CameraRail m_cameraRail;
+	Camera m_camera;
+	Transform nextPos;
+	float m_speed = 1f;
+
+	private void Awake() {
+		m_camera = Camera.main;
+	}
+	void Update () {
+		if(Input.GetButton("Fire1")) {
+			m_cameraRail.GetNextPosition(out nextPos);
+		}
+		if (transform.position != nextPos.position) {
+
+		}
+	}
+}
---
+++ FPSVRails/Assets/CameraRail.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRail : MonoBehaviour {
+	[SerializeField] Transform[] m_positions;
+	uint m_index = 0;
+
+	public bool GetNextPosition(out Transform requested) {
+		requested = m_positions[m_index];
+		if (m_index < m_positions.Length){
+			m_index++;
+			return true;
+		} else {
+			return false;
+		}
+	} 
+}
