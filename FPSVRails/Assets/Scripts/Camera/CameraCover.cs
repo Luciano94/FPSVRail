0bd0e80603fd9277ab3e3cf3b7a1bce711e8998f
Unknown <mmonterrosa94@gmail.com>
2018-08-28 17:56:01 -0300

    Start Camera Cover

---
+++ FPSVRails/Assets/Scripts/Camera/CameraCover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCover : MonoBehaviour {
+	[SerializeField] float m_speed;
+	[SerializeField] float m_height = 0.5f;
+	Vector3 m_targetPosition;
+
+	private void Update() {
+		if (Input.GetButtonDown("Jump")){
+			TakeCover();
+		}
+		m_targetPosition = new Vector3 (transform.position.x,
+			m_targetPosition.y,
+			transform.position.z);
+		transform.position = Vector3.Lerp(transform.position,
+			m_targetPosition, Time.deltaTime * m_speed);
+	}
+
+	void TakeCover() {
+		m_targetPosition = transform.position + Vector3.up * m_height;
+			transform.position = Vector3.Lerp(transform.position,
+				m_targetPosition, Time.deltaTime * m_speed);
+	}
+}
--- FPSVRails/Assets/Scripts/Camera/CameraRail.cs
+++ FPSVRails/Assets/Scripts/Camera/CameraRail.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 public class CameraRail : MonoBehaviour {
 	[SerializeField] Transform[] m_positions;
 	uint m_index = 0;
-
-	private void Awake() {
-
-	}
-
+	
 	public bool GetNextPosition(out Transform requested) {
 		if (m_index < m_positions.Length) {
 			requested = m_positions[m_index];
