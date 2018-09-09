53c4344d5ba6ae53f31de7b5499dbf89b653cff3
Unknown <lucho.elp40@gmail.com>
2018-09-09 16:35:56 -0300

    v0.6-Enemy AI Update
    -Added shots of enemies

--- FPSVRails/Assets/Scripts/Enemy/BulletScript.cs
+++ FPSVRails/Assets/Scripts/Enemy/BulletScript.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletScript : MonoBehaviour {
-
+	[SerializeField]private float speed;
+	[SerializeField]private float lifetime=5f;
 	// Use this for initialization
 	void Start () {
-		
+		Vector3 rndDirection = new Vector3(Random.Range(-5,5),Random.Range(-5,5),0);
+		transform.Rotate(rndDirection);
+		Destroy(gameObject,lifetime);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
 	}
 }
