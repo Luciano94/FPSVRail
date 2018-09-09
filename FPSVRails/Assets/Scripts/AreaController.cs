3fbc16ecc3d965dbbffbc74ee7e542f132c1937b
Marchin <mmonterrosa94@gmail.com>
2018-09-09 19:20:09 -0300

    v0.7-Automated Camera
    Camera moves to next position when enemies are deleted (using AreaController & EnemyArea)
    Now camera starts at the first point of the rail

---
+++ FPSVRails/Assets/Scripts/AreaController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaController : MonoBehaviour {
+    [SerializeField] List<EnemyArea> m_enemies;
+    [SerializeField] CameraMovement m_camMov;
+
+    private void Awake() {
+        foreach (EnemyArea enemy in m_enemies) {
+            enemy.SetAreaController(this);
+        }
+    }
+
+    public void Remove(EnemyArea enemy) {
+        m_enemies.Remove(enemy);
+        if (m_enemies.Count == 0) {
+            m_camMov.NextArea();
+        }
+    }
+
+}
---
+++ FPSVRails/Assets/Scripts/Enemy/EnemyArea.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class EnemyArea : MonoBehaviour {
+    AreaController m_areaController;
+
+    public void SetAreaController(AreaController areaController) {
+        m_areaController = areaController;
+    }
+
+    private void OnDisable() {
+        m_areaController.Remove(this);
+    }
+}
