2fa77e5ab67bdc9580cbd60ccc01c18bfc545517
Unknown <lucho.elp40@gmail.com>
2018-09-10 20:47:13 -0300

    v0.11-PlayerAim
    -Added General movement of player

---
+++ FPSVRails/Assets/Scripts/Camera/GeneralMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralMovement : MonoBehaviour {
+	[SerializeField] private float speed;
+    [SerializeField] private Vector2 minLimit;
+    [SerializeField] private Vector2 maxLimit;
+    private float  rotVert = 0f;
+    private float rotHor = 0f;
+
+    private void Awake() {
+        minLimit.x = transform.eulerAngles.x - minLimit.x;
+        minLimit.y = transform.eulerAngles.y - minLimit.y;
+        maxLimit.x += transform.eulerAngles.x;
+        maxLimit.y += transform.eulerAngles.y;
+    }
+
+    private void Update() {
+        Movement(); 
+    }
+
+    private void Movement(){
+        rotVert += Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        rotVert = Mathf.Clamp(rotVert , minLimit.x, maxLimit.x);
+        rotHor += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        rotHor = Mathf.Clamp(rotHor, minLimit.y, maxLimit.y);
+
+        transform.eulerAngles = new Vector3(-rotVert,rotHor,0);
+    }
+}
