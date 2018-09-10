133505ca3cbd79c144ef93e40c0cf7c1de47116b
Marchin <mmonterrosa94@gmail.com>
2018-09-10 19:18:23 -0300

    v0.10-Add End Scene

--- FPSVRails/Assets/Scripts/Camera/CameraRail.cs
+++ FPSVRails/Assets/Scripts/Camera/CameraRail.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class CameraRail : MonoBehaviour {
 	[SerializeField] Transform[] m_positions;
+	[SerializeField] LevelManager m_levelManager;
 	uint m_index = 0;
-	
+
 	public bool GetNextPosition(out Transform requested) {
 		if (m_index < m_positions.Length) {
 			requested = m_positions[m_index];
 			m_index++;
 			return true;
 		} else {
+			m_levelManager.NextLevel();
 			requested = m_positions[0];
-			print("Nos pasamos");
 			return false;
 		}
 	}
