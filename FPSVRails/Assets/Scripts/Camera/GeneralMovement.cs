bd117d2535fca89c86ea4c88a5fbb2d24e34123d
Unknown <lucho.elp40@gmail.com>
2018-09-11 17:11:41 -0300

    v0.9.7 - Camera Bug Fixed
    -La camara ahora toma los limites correctamente

--- FPSVRails/Assets/Scripts/Camera/GeneralMovement.cs
+++ FPSVRails/Assets/Scripts/Camera/GeneralMovement.cs
@@ -3,31 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GeneralMovement : MonoBehaviour {
-	[SerializeField] private float speed;
+    [SerializeField] private float speed;
     [SerializeField] private Vector2 minLimit;
     [SerializeField] private Vector2 maxLimit;
-    private float  rotVert = 0f;
+    private float rotVert = 0f;
     private float rotHor = 0f;
+    Vector3 rotation;
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         BuscarAngulos();
     }
-    private void Update() {
-        Movement(); 
+    private void Update()
+    {
+        Movement();
     }
 
-    public void BuscarAngulos() {
-        minLimit.x -= transform.eulerAngles.x;
-        minLimit.y = transform.eulerAngles.y + minLimit.y;
-        maxLimit.x += transform.eulerAngles.x;
-        maxLimit.y += transform.eulerAngles.y;
+    private void BuscarAngulos()
+    {
+        rotation = transform.localEulerAngles;
     }
-    private void Movement(){
+
+    private void Movement()
+    {
         rotVert += Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        rotVert = Mathf.Clamp(rotVert , minLimit.x, maxLimit.x);
+        rotVert = Mathf.Clamp(rotVert, minLimit.x, maxLimit.x);
         rotHor += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         rotHor = Mathf.Clamp(rotHor, minLimit.y, maxLimit.y);
-
-        transform.eulerAngles = new Vector3(-rotVert, rotHor,0);
+        Vector3 rot = new Vector3(-rotVert, rotHor, 0);
+        rot += rotation;
+        transform.localEulerAngles = rot;
     }
 }
