e09c79430a7a52d49d06b4639df1dd9ff0ea6cfc
Unknown <mmonterrosa94@gmail.com>
2018-09-18 15:39:23 -0300

    v0.12 - Add Pause Menu

---
+++ FPSVRails/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+	[SerializeField] Canvas m_HUD;
+	Canvas m_pauseMenu;
+
+	private void Awake() {
+		DontDestroyOnLoad(gameObject);
+		m_pauseMenu = GetComponent<Canvas>();
+		m_HUD.enabled = true;
+		m_pauseMenu.enabled = false;
+	}
+
+	void Update () {
+		if (Input.GetButtonDown("Submit")) {
+			Pause();
+		}	
+	}
+
+	void Pause() {
+		m_HUD.enabled = !m_HUD.isActiveAndEnabled;
+		m_pauseMenu.enabled = !m_pauseMenu.isActiveAndEnabled;
+		Time.timeScale = (m_pauseMenu.isActiveAndEnabled? 0f : 1f);
+	}
+}
