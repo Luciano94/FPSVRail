eb2c14530877fd21896c07e7440420d9429f9c32
Marchin <mmonterrosa94@gmail.com>
2018-10-01 20:22:26 -0300

    Add visual damage feedback

--- FPSVRails/Assets/Scripts/Managers/PlayerLife.cs
+++ FPSVRails/Assets/Scripts/Managers/PlayerLife.cs
@@ -1,23 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class PlayerLife : MonoBehaviour {
 
 	[SerializeField] private int health = 3;
 	[SerializeField] private Text healthTxt;
+	public UnityEvent damageTaken;
 
 	private void Awake() {
 		healthTxt.text = "Health: " + health.ToString();
 	}
 
-	public void TakeDamage() {	
-		if(health > 1){
+	public void TakeDamage() {
+		if (health > 1) {
 			health--;
-			healthTxt.text = "Health: " + health.ToString(); 
-		}
-		else{
+			healthTxt.text = "Health: " + health.ToString();
+			damageTaken.Invoke();
+		} else {
 			SceneManager.LoadScene("End");
 		}
 	}
---
+++ FPSVRails/Assets/Scripts/UI/DamageFeedback.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageFeedback : MonoBehaviour {
+    [SerializeField] PlayerLife m_player;
+    Image m_image;
+
+    private void Awake() {
+        m_image = GetComponent<Image>();
+        m_player.damageTaken.AddListener(TakeHit);
+    }
+
+    private void Update() {
+        float currAlpha = m_image.color.a;
+        if (currAlpha > 0f) {
+            SetAlpha(currAlpha - 0.05f);
+        }
+    }
+
+    void TakeHit() {
+        SetAlpha(0.8f);
+    }
+
+    void SetAlpha(float alpha) {
+        Color color = m_image.color;
+        color.a = alpha;
+        m_image.color = color;
+    }
+}
